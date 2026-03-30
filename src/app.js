544afd5c7a9d025aaa1dd1d676733a719b529018
akash-neeved <akash@neeved.com>
2026-03-31 00:08:06 +0530

    fix: add frontend uri

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   })
 );
